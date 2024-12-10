c00159937f4c441e0d154e847ef52a7665f3e261
Travis French <travis.french.1991@gmail.com>
2024-12-10 00:06:31 -0800

    [FEATURE] CRAWL-2 Initial commit.

---
+++ Assets/Tests/EditMode/PlaceholderTests.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace Attrition.Tests
+{
+    public class PlaceholderTests
+    {
+        [Test]
+        public void GivenNothing_WhenExecuted_ThenThisTestAlwaysPasses()
+        {
+            /* ARRANGE */
+            // Some setup action
+            
+            /* ACT */
+            // The action we are testing
+            
+            /* ASSERT */
+            // A list of assertions that must be true for the test to pass
+            Assert.Pass();
+        }
+    }
+}
