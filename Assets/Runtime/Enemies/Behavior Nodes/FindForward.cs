2fbc4da76fc3469cec917dc4cbe34011d83e2d07
Kylie-Lingo <157653967+Kylie-Lingo@users.noreply.github.com>
2024-12-19 11:42:53 -0800

    feature/CRAWL-80
    Reorganized enemy folder pathing
    Basics of Boar Behavior
    Basic prototype nodes for behavior for future use, and use in Boar.

---
+++ Assets/Runtime/Enemies/Behavior Nodes/FindForward.cs
@@ -0,0 +1,38 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+using Unity.VisualScripting;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "FindForward", story: "[Agent] sets [forward]", category: "Action/Blackboard", id: "91d53f3ab4694c6e9fcf6b912605936d")]
+public partial class FindForwardAction : Action
+{
+    [SerializeReference] public BlackboardVariable<GameObject> agent;
+    [SerializeReference] public BlackboardVariable<Vector3> forward;
+    private float timer;
+    [SerializeReference] public BlackboardVariable<float> delay;
+    protected override Status OnStart()
+    {
+        forward.Value = GameObject.transform.forward;
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        timer += Time.deltaTime;
+        if (timer >= delay.Value)
+        {
+            forward.Value = GameObject.transform.forward;
+            timer = 0;
+            return Status.Success;
+        }
+        return Status.Failure;
+    }
+
+    protected override void OnEnd()
+    {
+    }
+}
+
---
+++ Assets/Runtime/Enemies/Behavior Nodes/SetAttackForceAction.cs
@@ -0,0 +1,29 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "Set Attack Force", story: "Set force [rotation] by [power]", category: "Action/Physics", id: "0cb81013c8d434fda17d48d195d97b25")]
+public partial class SetAttackForceAction : Action
+{
+    [SerializeReference] public BlackboardVariable<Vector3> Rotation;
+    [SerializeReference] public BlackboardVariable<float> Power;
+
+    protected override Status OnStart()
+    {
+        Rotation.Value = new Vector3(Rotation.Value.x * Power, 0, Rotation.Value.z * Power);
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        return Status.Success;
+    }
+
+    protected override void OnEnd()
+    {
+    }
+}
+
