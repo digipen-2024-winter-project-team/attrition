312993864e41ad54f15dd3ceaba587f23609b2bc
Travis French <travis.french.1991@gmail.com>
2024-12-23 02:20:24 -0800

    Implemented ReadOnlyAttribute.

---
+++ Assets/Runtime/Common/ReadOnlyAttribute.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Attrition.Common
+{
+    /// <summary>
+    /// An attribute used to mark fields in Unity as read-only in the Inspector.
+    /// </summary>
+    public class ReadOnlyAttribute : PropertyAttribute
+    {
+        public ReadOnlyAttribute()
+        {
+        }
+    }
+}
