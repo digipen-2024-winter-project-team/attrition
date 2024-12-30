1d49a6955a9e9f3792bfe1ab56e85484cec80c14
Travis French <travis.french.1991@gmail.com>
2024-12-29 19:33:23 -0800

    Fixed a bug where character selection options would disappear in subsequent selections.

--- Assets/Runtime/CharacterClasses/CharacterClassRandomizer.cs
+++ Assets/Runtime/CharacterClasses/CharacterClassRandomizer.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Attrition.Common;
 using Attrition.Common.Picking;
 using Attrition.Common.Picking.Builder;
@@ -18,27 +20,47 @@ namespace Attrition.CharacterClasses
         private List<CharacterClass> availableClasses;
         private CharacterClassBehaviour classBehaviour;
         private Picker<CharacterClass> picker;
-
-        private static readonly List<CharacterClass> AlreadyPicked = new();
         
+        private static readonly List<CharacterClass> AlreadyPicked = new();
+        private static bool wasCleared;
+
         private void Awake()
         {
             this.classBehaviour ??= this.GetComponent<CharacterClassBehaviour>();
+        }
+
+        private void OnEnable()
+        {
+            wasCleared = false;
             
             var builder = new PickerBuilder<CharacterClass>();
             this.picker = builder
                 .UseRandom()
                 .ExcludeAnyIn(AlreadyPicked)
                 .Build();
-        }
-
-        private void OnEnable()
-        {
+            
             var shouldRandomize = this.overwriteExisting || this.classBehaviour.CharacterClass == null;
 
             if (this.randomizeOnEnable && this.classBehaviour != null && shouldRandomize)
             {
                 this.Randomize();
+            }
+        }
+
+        private void OnDisable()
+        {
+            // So this is a bit gross. We want to share state between all randomizers so that each one can pick
+            // a unique class. Ideally this should be broken into a separate (Scriptable?) object that can be shared
+            // between all randomizers. For now, I'm using statics.
+            // 
+            // The problem is that static data is shared between instances of the same type - across the entire
+            // application lifecycle. This means that we need to carefully manage this static state to ensure that
+            // when the selection menu is entered again, we don't have carry-over from the previous selection.
+            
+            if (!wasCleared)
+            {
+                AlreadyPicked.Clear();
+                wasCleared = true;
             }
         }
 
