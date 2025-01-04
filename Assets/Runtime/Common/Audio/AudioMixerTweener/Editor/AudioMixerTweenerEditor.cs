afa00744becc35139a0796e29b54fe3e8c569ee1
Travis French <travis.french.1991@gmail.com>
2025-01-03 16:18:26 -0800

    Set up mixer tweening based trigger volumes.

--- Assets/Runtime/Common/Audio/AudioMixerTweener/Editor/AudioMixerTweenerEditor.cs
+++ Assets/Runtime/Common/Audio/AudioMixerTweener/Editor/AudioMixerTweenerEditor.cs
@@ -27,79 +27,11 @@ namespace Attrition.Common.Audio.AudioMixerTweener.Editor
         public override VisualElement CreateInspectorGUI()
         {
             this.audioMixerTweener = target as AudioMixerTweener;
-            var parameterNames = GetExposedParameterNames(this.target);
-
-            foreach (var parameterName in parameterNames)
-            {
-                Debug.Log($"{parameterName}");
-            }
             
+            // Was going to make a custom inspector so that the ExposedParameters could be a DropdownField but
+            // Unity really likes sealed internal classes.
+
             return this.root;
-        }
-
-        public static List<string> GetExposedParameterNames(object audioMixerTweener)
-        {
-            var parameterNames = new List<string>();
-
-            if (audioMixerTweener == null)
-            {
-                Debug.LogWarning("AudioMixerTweener instance is null.");
-                return parameterNames;
-            }
-
-            // Use reflection to access the private "audioMixer" field from AudioMixerTweener
-            var audioMixerField = audioMixerTweener.GetType()
-                .GetField("audioMixer", BindingFlags.Instance | BindingFlags.NonPublic);
-
-            if (audioMixerField != null)
-            {
-                var audioMixer = audioMixerField.GetValue(audioMixerTweener) as AudioMixer;
-
-                if (audioMixer != null)
-                {
-                    // Use reflection to access the private method "GetExposedParameters" from AudioMixer
-                    var getParameterNamesMethod = typeof(AudioMixer)
-                        .GetMethod("GetExposedParameters", BindingFlags.Instance | BindingFlags.NonPublic);
-
-                    if (getParameterNamesMethod != null)
-                    {
-                        // Retrieve the exposed parameters from the AudioMixer
-                        var parameters = (object[])getParameterNamesMethod.Invoke(audioMixer, null);
-
-                        if (parameters != null)
-                        {
-                            foreach (var parameter in parameters)
-                            {
-                                // Access the "name" field of the exposed parameter
-                                var nameField = parameter.GetType().GetField("name", BindingFlags.Instance | BindingFlags.Public);
-
-                                if (nameField != null)
-                                {
-                                    var name = nameField.GetValue(parameter) as string;
-                                    if (!string.IsNullOrEmpty(name))
-                                    {
-                                        parameterNames.Add(name);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Debug.LogError("Failed to access GetExposedParameters method via reflection.");
-                    }
-                }
-                else
-                {
-                    Debug.LogError("Failed to retrieve AudioMixer from AudioMixerTweener.");
-                }
-            }
-            else
-            {
-                Debug.LogError("AudioMixerTweener does not have a private 'audioMixer' field.");
-            }
-
-            return parameterNames;
         }
     }
 }
